11a6c06d312b5b622b1c8f947b64e3f6bda1b721
Jose Lopez <jose.lopezgarrido95@gmail.com>
2023-09-01 22:02:37 -0400

    code cleanup

--- src/main/java/com/glist/GroceriesList/service/AuthenticationService.java
+++ src/main/java/com/glist/GroceriesList/service/AuthenticationService.java
@@ -4,27 +4,20 @@ import com.glist.GroceriesList.model.request.AuthenticationRequest;
 import com.glist.GroceriesList.model.request.RegisterRequest;
 import com.glist.GroceriesList.model.response.Response;
 import com.glist.GroceriesList.model.response.UserAuthenticationResponse;
-import com.glist.GroceriesList.repository.ContainerDbRepository;
 import com.glist.GroceriesList.repository.UserRepo;
 import com.mongodb.MongoWriteException;
-import io.jsonwebtoken.Claims;
 import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.nio.file.AccessDeniedException;
-import java.time.Duration;
-import java.time.Instant;
-import java.util.Date;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class AuthenticationService {
     private final CookieService cookieService;
-    private final JwtService jwtService;
     private final UserRepo userRepo;
 
     public UserAuthenticationResponse register(RegisterRequest req) throws MongoWriteException {
@@ -51,18 +44,22 @@ public class AuthenticationService {
     }
 
     //TODO: Needs implementing
-    public Boolean refreshToken(String token) throws Exception {
-        if (token == null) throw new AccessDeniedException("No token found.");
-        Date expirationDate = jwtService.extractClaim(token, Claims::getExpiration);
-        Instant instantToCheck = expirationDate.toInstant();
-        Instant currentInstant = Instant.now();
-        Duration duration = Duration.between(instantToCheck, currentInstant);
-        // refresh if within 24hrs
-        return duration.toHours() <= 24;
-    }
+//    public Boolean refreshToken(String token) throws Exception {
+//        if (token == null) throw new AccessDeniedException("No token found.");
+//        Date expirationDate = jwtService.extractClaim(token, Claims::getExpiration);
+//        Instant instantToCheck = expirationDate.toInstant();
+//        Instant currentInstant = Instant.now();
+//        Duration duration = Duration.between(instantToCheck, currentInstant);
+//        // refresh if within 24hrs
+//        return duration.toHours() <= 24;
+//    }
 
     public Cookie logout(String token) {
-        // TODO: Expire token
+        // TODO: Expire token if token exists
+        if (token != null) {
+            log.debug(token);
+        }
+        // Return a logout cookie
         return cookieService.deleteAuthCookie();
     }
 }
